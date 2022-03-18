b666cb356cc343fd735da1a63ff30736281ee269
Rostyslav <malanyukrostik@gmail.com>
2022-03-18 21:30:41 +0200

    edit settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -139,8 +139,8 @@ REST_FRAMEWORK = {
 #smtp
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'malanyukrostik@gmail.com'
-EMAIL_HOST_PASSWORD = '12345Fdl'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'Password'
 EMAIL_PORT = 587
 
 
